76b141da7c0fa41ea03235c3ec5b0c7c8b09bd37
MkshSahani <msco18333@gmail.com>
2021-01-18 16:49:50 +0530

    Linked List started

---
+++ LinkedList/cpp/LinkedListNode.h
@@ -0,0 +1,77 @@
+#ifndef LINKEDLISTNODE_H 
+#define LINKEDLISTNODE_H 
+
+/**
+ * @class : SinlyLinkedListNode 
+ * 
+*/
+
+template <typename T> 
+class SinglyLinkedListNode {
+    public: 
+
+        SinglyLinkedListNode(); 
+        SinglyLinkedListNode(T data); 
+        T getData() const; 
+        void setData(T data); 
+        SinglyLinkedListNode<T>* getNext() const; 
+        void setNext(SinglyLinkedListNode<T> *next_node); 
+
+    private: 
+        T data; 
+        SinglyLinkedListNode *next; 
+     
+}; 
+
+
+template <typename T> 
+SinglyLinkedListNode<T>::SinglyLinkedListNode() {
+    this->next = NULL; 
+}
+
+template <typename T>
+SinglyLinkedListNode<T>::SinglyLinkedListNode(T data) {
+    this->data = data; 
+    this->next = NULL; 
+} 
+
+template <typename T> 
+void SinglyLinkedListNode<T>::setData(T data) {
+    this->data = data; 
+}
+
+template <typename T> 
+T SinglyLinkedListNode<T>::getData() const 
+{
+    return this->data; 
+}
+
+template <typename T> 
+void SinglyLinkedListNode<T>::setNext(SinglyLinkedListNode<T> *next_node) {
+    this->next = next_node; 
+}
+
+template <typename T> 
+SinglyLinkedListNode<T>* SinglyLinkedListNode<T>::getNext() const {
+    return this->next; 
+}
+
+
+
+/**
+ * @class : DoublyLinkedListNode. 
+*/
+
+// template <typename T> 
+// class DoublyLinkedListNode : public SinlgyLinkedListNode<T> {
+//     public: 
+//         DoublyLinkedListNode(); 
+//         DoublyLinkedListNode(T data); 
+
+//     private: 
+//         DoublyLinkedListNode* previous; 
+
+// }; 
+
+
+#endif 
